acc2add5394793831d6cfd3f9dfc0f682e26859d
ralph freijy <rfreijy@gmail.com>
2026-06-10 10:39:51 +0300

    updated

--- flight-passanger-system/routes/api.php
+++ flight-passanger-system/routes/api.php
@@ -11,21 +11,13 @@ Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:lo
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::apiResource('/passengers', PassengerController::class)->only([
-        'index',
-        'show',
-    ]);
-
     Route::apiResource('/flights', FlightController::class)->only([
         'index',
         'show',
     ]);
 
     Route::middleware('role:admin')->group(function () {
-        Route::apiResource('/passengers', PassengerController::class)->except([
-            'index',
-            'show',
-        ]);
+        Route::apiResource('/passengers', PassengerController::class);
 
         Route::apiResource('/flights', FlightController::class)->except([
             'index',
@@ -34,11 +26,10 @@ Route::middleware('auth:sanctum')->group(function () {
 
         Route::apiResource('/users', UserController::class);
 
-
         Route::get('/users-export', [UserController::class, 'export']);
         Route::get('/admin/profile', [AuthController::class, 'profile']);
 
         Route::post('/flights/{flight}/passengers/{passenger}', [FlightController::class, 'assignPassenger']);
         Route::delete('/flights/{flight}/passengers/{passenger}', [FlightController::class, 'unassignPassenger']);
-     });
+    });
 });
